736f4b70a76631c3aff6a9ebfc38f75989b67ee7
Nishant Singh <nishan.singh0199@gmail.com>
2022-10-11 04:52:52 +0530

    day2 assignment

---
+++ Day2_problem/conditional-statements-IW/problem3.js
@@ -0,0 +1,12 @@
+
+let a=31;
+let b=13;
+
+if(a>b){
+  console.log("a is greater")
+}else if(a<b){
+  console.log("b is greater")
+}else if(a==b){
+  console.log("both equal")
+}
+  
---
+++ Day2_problem/conditional-statements-IW/problem4.js
@@ -0,0 +1,13 @@
+let username="nishant@masai.com";
+let pwd="MasaI@23";
+
+if(username==="nishant@masai.com"){
+  if(pwd==="MasaI@23"){
+    console.log("you are logged in")
+  }else{
+    console.log("Password incorrect")
+  }
+}else{
+  console.log("Username incorrect")
+}
+  
